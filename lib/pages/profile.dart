b82b13231b331322573e4bd2e38d8dced7da8385
ffm-id <ferialfahri@gmail.com>
2022-07-01 17:39:33 +0100

    Test to add update profile functionality (locally/without connecting to the api

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../pages/edit_name.dart';
+import '../widgets/display_image_widget.dart';
+import '../user/user_data.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -10,13 +15,94 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final user = UserData.myUser;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile',
-        style: TextStyle(fontSize: 50),
-      ),
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(children: [
+        AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          toolbarHeight: 10,
+        ),
+        Center(
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Text("Edit Profile",
+                style: GoogleFonts.bebasNeue(fontSize: 35)),
+          ),
+        ),
+        InkWell(
+          onTap: () {},
+          child: DisplayImage(
+            imagePath: user.image,
+            onPressed: () {},
+          ),
+        ),
+        SizedBox(height: 35),
+        buildUserInfoDisplay(user.name, 'Name', EditNameFormPage()),
+      ]),
     );
+  }
+
+  // Widget builds the display item with the proper formatting to display the user's info
+  Widget buildUserInfoDisplay(String getValue, String title, Widget editPage) =>
+      Padding(
+          padding: EdgeInsets.only(bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Container(
+                width: 350,
+                height: 40,
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(
+                  color: Colors.grey,
+                  width: 1,
+                ))),
+                child: Row(children: [
+                  Expanded(
+                      child: TextButton(
+                          onPressed: () {
+                            navigateSecondPage(editPage);
+                          },
+                          child: Text(
+                            getValue,
+                            style: TextStyle(
+                                fontSize: 20, height: 1.4, color: Colors.black),
+                          ))),
+                  Icon(
+                    Icons.keyboard_arrow_right,
+                    color: Colors.grey,
+                    size: 40.0,
+                  )
+                ]),
+              )
+            ],
+          ));
+
+  // Refresh the page after updating user info.
+  FutureOr onGoBack(dynamic value) {
+    setState(() {});
+  }
+
+  // Handles navigation and prompts refresh.
+  void navigateSecondPage(Widget editForm) {
+    Route route = MaterialPageRoute(builder: (context) => editForm);
+    Navigator.push(context, route).then(onGoBack);
   }
 }
