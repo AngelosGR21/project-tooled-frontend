20618466749c3e68cbcd8aa4910bdea7e0aecc33
AngelosGR21 <angelgrigoriu@gmail.com>
2022-07-06 13:49:49 +0100

    connecting backend with frontend

---
+++ lib/Utils/ws.dart
@@ -0,0 +1,22 @@
+import 'package:web_socket_channel/io.dart';
+import "../globals.dart" as globals;
+import 'dart:convert';
+
+void sendMessage(msg) {
+  IOWebSocketChannel? channel;
+  try {
+    channel = IOWebSocketChannel.connect("ws://localhost:5000");
+  } catch (e) {
+    print("Error connecting to websocket: " + e.toString());
+  }
+  var sendDetails = jsonEncode({"message": msg, "token": globals.token});
+  channel?.sink.add(sendDetails);
+
+  channel?.stream.listen((event) {
+    if (event!.isNotEmpty) {
+      print("This is what came back >>>> $event");
+    }
+
+    channel!.sink.close();
+  });
+}
--- lib/pages/inbox.dart
+++ lib/pages/inbox.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import "../Utils/ws.dart";
 
 class Inbox extends StatefulWidget {
   const Inbox({Key? key}) : super(key: key);
@@ -10,13 +10,29 @@ class Inbox extends StatefulWidget {
 }
 
 class _InboxState extends State<Inbox> {
+  String _message = "";
+  // print(globals.token);
+  // print(msg);
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Inbox',
-        style: TextStyle(fontSize: 50),
-      ),
-    );
+        child: Column(
+      children: [
+        Center(
+          widthFactor: 30,
+          child: TextField(
+            onChanged: (e) => _message = e,
+          ),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              if (_message.isNotEmpty) {
+                sendMessage(_message);
+              }
+            },
+            child: Text("Send message"))
+      ],
+    ));
   }
 }
